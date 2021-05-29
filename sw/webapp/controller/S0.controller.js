76d10a536079b85e9fb5d4611a7604413f77cdaa
Raul Romano Mendonça <raulromanosap@hotmail.com>
2021-05-29 14:43:00 +0100

    create new model and updating value control in the call api

--- sw/webapp/controller/S0.controller.js
+++ sw/webapp/controller/S0.controller.js
@@ -10,15 +10,18 @@ sap.ui.define([
 
 		return Controller.extend("sw.controller.S0", {
 			onInit: function () {
-				this.sValueSearchField = this.byId("idSearch");
 				this.urlApi = "https://swapi.dev/api/people/";
 				this.oModel = new JSONModel();
 				this.getView().setModel(this.oModel);
+
+				this.oModelControl = new JSONModel();
+				this.getView().setModel(this.oModelControl, "control");
 			},
 
 			onChange: function (oEvent) {
-				this.sValueSearchField = oEvent.getSource().getValue();
-				this.oModel.loadData(this.urlApi + this.sValueSearchField + "/");
+				var sValueSearchField = this.oModelControl.getProperty("/searchField");
+				//this.sValueSearchField = oEvent.getSource().getValue();
+				this.oModel.loadData(this.urlApi + sValueSearchField + "/");
 			},
 
 			onDocumentation: function(){
